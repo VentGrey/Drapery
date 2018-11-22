fb41eef019269107b32be4a375497101bf8ac5d9
VentGrey <omarpurataf@gmail.com>
2018-11-22 17:44:49 -0600

    Added navbar menu

--- student-panel-groups.php
+++ student-panel-groups.php
@@ -47,6 +47,9 @@
         <a class="navbar-brand" href="#">
             <img src="img/Drapery-text.png" height="40px" alt="The Drapery Falls">
         </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
     </nav>
   </body>
 </html>
