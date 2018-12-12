2eadbb8d14e9ea7919053fb1e674f4eb3002ec88
VentGrey <omarpurataf@gmail.com>
2018-12-12 13:55:19 -0600

    Corrected comments & added SMTP

--- PHP/send-link.php
+++ PHP/send-link.php
@@ -4,10 +4,10 @@
 //Cambie estos campos a su conveniencia para el uso de Drapery
 
 if(isset($_POST['submit-email']) && $_POST['email']) {
-    //Change this according to your database location & user
+    //Cambie esto dependiendo de su DBMS y su usuario
     mysql_connect('localhost', 'root','');
-    mysql_select_db('sample'); //Change this to your current database
-    $select = mysql_query(""); //Place your queries here
+    mysql_select_db('sample'); //Coloque aquí su base de datos
+    $select = mysql_query(""); //Las consultas van aquí
 
     if(mysql_num_rows($select) == 1) {
         while($row = mysql_fetch_array($select)) {
@@ -15,8 +15,21 @@ if(isset($_POST['submit-email']) && $_POST['email']) {
             $pass = md5($row['password']);
         }
         $link = 'add your link button here';
-        require_once('phpmail/PHPMailerAutoload.php'); //This can vary
+        require_once('phpmail/PHPMailerAutoload.php'); //Esto puede variar
         $mail = new PHPMailer();
         $mail -> CharSet = "utf-8";
+        $mail -> IsSMTP();
+        // Autenticación por SMTP
+        $mail -> SMPAuth = true;
+
+        // Ponga sus datos aquí
+        $mail -> Username = "example@example.exa";
+
+        $mail -> Password = "superstrongpassword";
+        $mail -> SMTPSecure = "ssl";
+
+        // ELija su servidor SMTP, el default es gmail
+        $mail -> Host = "smtp.gmail.com";
+
     }
 }
