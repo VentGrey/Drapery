97169369bb3db6f20ddf477a9a8e636927b8b241
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-12-01 01:39:49 -0600

    Added common scripts.php

---
+++ scripts.php
@@ -0,0 +1,28 @@
+<!-- Bootstrap common scripts -->
+
+<!-- JQuery -->
+<script type="text/javascript" src="js/jquery.min.js"></script>
+<!-- Bootstrap tooltips -->
+<script type="text/javascript" src="js/popper.min.js"></script>
+<!-- Bootstrap core JavaScript -->
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- MDB core JavaScript -->
+<script type="text/javascript" src="js/mdb.min.js"></script>
+
+<!-- Addons -->
+<script type="text/javascript" src="js/addons/mdb.min.js"></script>
+<script type="text/javascript" src="js/addons/datatables-select.min.js"></script>
+<script type="text/javascript" src="js/addons/datatables.min.js"></script>
+<script type="text/javascript" src="js/addons/directives.min.js"></script>
+<script type="text/javascript" src="js/addons/flag.min.js"></script>
+<script type="text/javascript" src="js/addons/imagesloaded.pkgd.min.js"></script>
+<script type="text/javascript" src="js/addons/jquery.zmd.hierarchical-display.min.js"></script>
+<script type="text/javascript" src="js/addons/masonry.pkgd.js"></script>
+<script type="text/javascript" src="js/addons/rating.min.js"></script>
+
+<!-- Modules -->
+<script type="text/javascript" src="js/modules/animations-extended.min.js"></script>
+<script type="text/javascript" src="js/modules/forms-free.min.js"></script>
+<script type="text/javascript" src="js/modules/scrolling-navbar.min.js"></script>
+<script type="text/javascript" src="js/modules/treeview.min.js"></script>
+<script type="text/javascript" src="js/modules/wow.min.js"></script>
