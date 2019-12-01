f3666a1ecc179159df20d789885c36b7b4e8a204
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-12-01 01:20:25 -0600

    Added a common includes file

---
+++ includes.php
@@ -0,0 +1,10 @@
+<!-- Bootstrap + local files -->
+<link rel="stylesheet" href="css/bootstrap.min.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/mdb.min.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
+
+<!-- Animations CSS -->
+<link rel="stylesheet" href="css/animations-extended.css" type="text/css" media="screen" />
+
+<!-- Icons (This requires an active internet connection) -->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
