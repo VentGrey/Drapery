a21d8556e72d86f1656035ae4490a1faaa6f473e
Omar Jair Purata Funes <omarpurataf@gmail.com>
2019-12-01 01:56:23 -0600

    Corrected CSS includes

--- includes.php
+++ includes.php
@@ -4,7 +4,7 @@
 <link rel="stylesheet" href="css/style.css" type="text/css" media="screen" />
 
 <!-- Animations CSS -->
-<link rel="stylesheet" href="css/animations-extended.css" type="text/css" media="screen" />
+<link rel="stylesheet" href="css/animations-extended.min.css" type="text/css" media="screen" />
 
 <!-- Icons (This requires an active internet connection) -->
 <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
